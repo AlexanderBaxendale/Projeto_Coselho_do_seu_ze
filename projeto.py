645ecb3c658e23f1fa785eb0e863160ac140711b
AlexanderBaxendale <alexaugustofig@hotmail.com>
2024-12-02 02:48:10 -0300

    esquecir de salvar

--- projeto.py
+++ projeto.py
@@ -3,51 +3,113 @@ from deep_translator import GoogleTranslator
 from loguru import logger
 
 
-
-
 def obter_conselho():
 
     r = requests.get('https://api.adviceslip.com/advice')
     if r.status_code == 200:
-        return r.json()['slip']['advice']
+        return r.json()['slip']['advice'], r.json()['slip']['id']
     else:
-        return "Erro ao obter conselho"
+        return "Erro ao obter conselho"    
 
-if __name__== '__main__':
-    obter_conselho()
+def gerar_conselho():
+     
+        conselho, conselho_id = obter_conselho()
+        if obter_conselho():
+            conselhos.append((conselho, conselho_id))
+            print(f' ID: {conselho_id}  -  conselho: {conselho}')  
+    
+    
+def guarda_conselho(conselhos):
+    with open('arquivo.txt', 'w', encoding='UTF-8') as arquivo:
+        for conselho, conselho_id in conselhos:
+            arquivo.write(f'ID: {conselho_id} - {conselho}')
+
+def menu():
+    print('\n-------Escolha entre uma das opções abaixo-------\n')
+    print('1 - Ouvir o Seu Zé -')
+    print('2 - Mostra Conselho -')
+    print('3 - Guardar a Sabedoria -')
+    print('4 - Mostra conselhos guardados -')
+    print('5 - Traduzir conselhos -')
+    print('6 - Traduzir conselhos salvos -')
+    print('7 - Sair do programa -')
+    print('\n-------------------------------------------------\n')
 
 
+print('\n----------------------------------------------------------------\n')
+print('                 Bem-Vindo a Cachaçaria do Seu Zé!                  ')
+print('                 A Melhor cachaçaria em dar conselhos               ')
+print('\n----------------------------------------------------------------\n')
 
-def numero_de_conselhos():
+escolha = 1000
 
-    tradutor = GoogleTranslator(source="en", target= "pt")
+while escolha != 0:
+    menu()
     try:
-        quantidade = int(input('Quantos conselhos você quer?:'))
-        if quantidade <= 0:
-            print('Digite um número positivo')
-            return
+        escolha = int(input('Digite a opção desejavel:'))
     except ValueError:
-        print('Digite uma Número válido!')
-        return
-    
-    for i in range(quantidade):
-        print(f'conselho {i+1} : {tradutor.translate(obter_conselho())}')
-if __name__ == '__main__':
-    numero_de_conselhos()
+        print('\n Digite uma valor valido \n')
 
+    match escolha:
+        case 1:
+            print('\n------------> QUANTIDADE DE CONSELHOS <------------\n')
+            try:
+                quantidade_conselho = int(input('Quantos conselhos você quer ?: '))
+            except ValueError :
+                print('digite um número valido!')
+            
+            conselhos = []
+            print('\nescolha a opção 2 para ver os seu conselhos\n')
+        case 2:
+            print('\n-------------> MOSTRA CONSELHO <-------------------\n')
+            try:
+                for i in range(quantidade_conselho):
+                    gerar_conselho()
+            except Exception as erro:
+                print(f'\nOcorreu um error: {erro} ! - Por favor digite a primeiro  opção 1 antes dessa \n')
 
-
-def guarda_conselho():
-    
-    gera_conselho= requests.get('https://api.adviceslip.com/advice')      
-    try:
-        gera_conselho == 200
-        return numero_de_conselhos()
-    except ValueError as error:
-        logger.exception(f'Error:{error}')
-    
-    with open('Conselhos.txt', 'w', encoding='UTF-8') as conselhos_salvo:
-        conselhos_salvo.write(gera_conselho.text)
-
-if __name__ == '__main__':
-    guarda_conselho()
+           
+        case 3 :
+            print('\n-------------> MOSTRA CONSELHO GUARDADO <-------------------\n')
+            try:
+                salvar = input('Gostaria de salvar seu conselho(s) - Digite s/n :')
+            except ValueError:
+                print('\nDigite um valor valido - Porfavor segua a ordem! \n')   
+              
+            if salvar == 's':
+                guarda_conselho(conselhos)
+                print('Seu Conselho está salvo!')
+            elif salvar == 'n':
+                print('Você decidio não salvar seu conselho')
+            else:
+                print('digite um valor valido')
+            
+            
+        case 4:
+            print('\n-------------> ABRIR LISTA DE CONSELHOS <-------------------\n')
+            try:
+                decicao = input('Gostaria de ver os sua lista de conselho(s) - Digite s/n :')
+            except ValueError:
+                print('Digite um valor valido')
+            
+            try:
+                if decicao == 's' :   
+                    with open('arquivo.txt', 'r', encoding='UTF-8') as arquivo:
+                        mostra_conselho = arquivo.read()
+                        print(mostra_conselho) 
+                else:
+                    print('Você decidio não ver sua lista de conselho(s)')
+            except FileNotFoundError:
+                print('O arquivo não foi encontrado')
+            
+        case 5:
+            
+            tradutor = GoogleTranslator(source="auto", target="pt")
+            print(tradutor.translate(conselhos))
+        case 7:
+            sair = input('Deseja sair do Programa - Digite s/n :')
+            if sair == 's':
+                print('\nobrigado por passa na Cachaça do seu Zé - Até a próxima\n')
+                break
+            elif sair == 'n':
+                print('\nVocê decidio fica para mais uma rodada\n')
