0ccffd751655bab110e61946253539e4e3767d74
AlexanderBaxendale <alexaugustofig@hotmail.com>
2024-12-02 16:26:16 -0300

    Adicionando o sleep

--- projeto.py
+++ projeto.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from deep_translator import GoogleTranslator
 from loguru import logger
 
@@ -25,6 +26,16 @@ def gerar_conselho():
             else:
                 print('valor incorreto')
 
+def abrir_conselho():
+    try:
+        if decicao == 's' :   
+            with open('arquivo.txt', 'r', encoding='UTF-8') as arquivo:
+                mostra_conselho = arquivo.read()
+                print(mostra_conselho) 
+        else:
+            print('Você decidio não ver sua lista de conselho(s)')
+    except FileNotFoundError:
+            print('Ainda não há conselhos guardados')
 
     
 def guarda_conselho(conselhos):
@@ -57,25 +68,27 @@ while escolha != 0:
         escolha = int(input('Digite a opção desejavel:'))
     except ValueError:
         print('\n Digite uma valor valido \n')
-
+        time.sleep(0.5)
     match escolha:
         case 1:
             print('\nX------------> QUANTIDADE DE CONSELHOS <------------X\n')
             try:
                 quantidade_conselho = int(input('Quantos conselhos você quer ?: '))
+                print('\nescolha a opção 2 para ver os seu conselhos\n')
             except ValueError :
-                print('digite um número valido!')
-            
+                print('\ndigite um número valido!')
+            time.sleep(0.5)
             conselhos = []
-            print('\nescolha a opção 2 para ver os seu conselhos\n')
+            
         case 2:
             print('\nX-------------> MOSTRA CONSELHO <-------------------X\n')
             conselhos_traduzidos = []
             try:
                 for i in range(quantidade_conselho):
                     gerar_conselho()
+                    time.sleep(1)
             except Exception as erro:
-                print(f'\nOcorreu um error: {erro} ! - Por favor digite a opção 1 antes dessa \n')
+                print(f'Ocorreu um error: {erro} ! - Por favor digite a opção 1 antes dessa ')
 
            
         case 3 :
@@ -83,15 +96,18 @@ while escolha != 0:
             try:
                 salvar = input('Gostaria de salvar seu conselho(s) - Digite s/n :')
             except ValueError:
-                print('\nDigite um valor valido! \n')   
+                print('\nDigite um valor valido!\n ')   
             try:  
                 if salvar == 's':
                     guarda_conselho(conselhos)
                     print('Seu Conselho está salvo!')
+                    time.sleep(0.5)
                 elif salvar == 'n':
                     print('Você decidio não salvar seu conselho')
+                    time.sleep(0.5)
                 else:
                     print('digite um valor valido')
+                    time.sleep(0.5)
             except Exception as erro :
                 print(f'Ocorreu um error : {erro} - O conselho ainda não foi gerado')
             
@@ -100,23 +116,17 @@ while escolha != 0:
             try:
                 decicao = input('Gostaria de ver os sua lista de conselho(s) - Digite s/n :')
             except ValueError:
-                print('Digite um valor valido')
+                print('\nDigite um valor valido\n')
+                time.sleep(0.5)
             
-            try:
-                if decicao == 's' :   
-                    with open('arquivo.txt', 'r', encoding='UTF-8') as arquivo:
-                        mostra_conselho = arquivo.read()
-                        print(mostra_conselho) 
-                else:
-                    print('Você decidio não ver sua lista de conselho(s)')
-            except FileNotFoundError:
-                print('Ainda não há conselhos guardados')
-            
+            abrir_conselho()
+            time.sleep(1)
+              
         case 5:
             tradutor = GoogleTranslator(source='en', target='pt')
             
             try:
-                mostra = input('Gostaria de ver seus conselhos Traduzidos - Digite s/n :')    
+                mostra = input('\nGostaria de ver seus conselhos Traduzidos - Digite s/n :\n')    
             except ValueError:
                 print('Digite uma valor valido')
             try:
@@ -124,25 +134,29 @@ while escolha != 0:
                     print(conselhos_traduzidos)
                 else:
                     print('você decidiu não ver seus conselhos traduzidos')
+                    time.sleep(0.5)
             except Exception as erro:
                 print(f'Ocorreu um error: {erro} -  Talvez os conselhos ainda não foram gerados')
         case 6:
-            
             try:
                 mostra_traducao = input('Aproveitando, você gostaria de salvar seus conselhos Traduzidos em um arquivo? - Digite s/n :')
             except ValueError:
-                print('digite um valor valido')
+                print('\ndigite um valor valido\n')
+                time.sleep(0.5)
             try:
                 if mostra_traducao == 's':
                     with open('arquivo.txt', 'w', encoding='UTF-8') as arquivo:
                         for conselho, conselho_id in conselhos:
                             arquivo.write(f'ID: {conselho_id} - {tradutor.translate(conselho)}')
+                    print('Seu conselho estar Salvo!')
+                    time.sleep(0.5)
                 else:
                     print('você decidiu não salvar seus conselhos traduzidos')
             except Exception as erro:
                     print(f'Ocorreu um error: {erro} -  Talvez os conselhos ainda não foram gerados') 
         case 7:
             sair = input('Deseja sair do Programa - Digite s/n :')
+            time.sleep(0.5)
             if sair == 's':
                 print('\nobrigado por passa na Cachaça do seu Zé - Até a próxima\n')
                 break
